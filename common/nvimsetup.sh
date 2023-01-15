797ae1812e1e21fa5036ccbbbbc400b20da01c2a
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-01-15 18:11:26 +0200

    nvim: Add theme

--- common/nvimsetup.sh
+++ common/nvimsetup.sh
@@ -16,6 +16,11 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo ' '
 
+echo 'Installing the theme'
+echo ''
+git clone https://github.com/tomasiser/vim-code-dark ~/.vim/bundle/vim-code-dark
+echo ''
+
 echo 'Installing vim-airline'
 echo ''
 git clone --branch v0.10 --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
