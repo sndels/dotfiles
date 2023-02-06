4539ab3739c6060762deb1dfd9ff826b6acdf78a
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-02-06 21:42:02 +0200

    Ignore phased updates in i3status apt indicator

--- linux/setup.sh
+++ linux/setup.sh
@@ -61,9 +61,9 @@ sudo gpasswd -a $USER video
 
 echo '==================== Install i3status-rs from source ====================='
 echo ''
-git clone https://github.com/greshake/i3status-rust
+git clone https://github.com/sndels/i3status-rust
 cd i3status-rust
-git checkout v0.22.0
+git checkout v0.22.0-ignore-phased
 sudo apt install -y libssl-dev libsensors-dev libpulse-dev
 cargo install --path . --locked
 ./install.sh
