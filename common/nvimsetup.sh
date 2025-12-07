5a350848cbdd6aeaed73cd70f9ac09f44b94626f
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2025-12-07 10:52:28 +0200

    nvim: Remove redundant manual installs

--- common/nvimsetup.sh
+++ common/nvimsetup.sh
@@ -22,19 +22,3 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo ' '
 
-echo 'Installing lspconfig'
-echo ''
-git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig
-echo ''
-
-echo 'Installing the theme'
-echo ''
-git clone https://github.com/tomasiser/vim-code-dark ~/.vim/bundle/vim-code-dark
-echo ''
-
-echo 'Installing vim-airline'
-echo ''
-git clone --branch v0.10 --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone --depth=1 https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-echo ''
-echo ''
